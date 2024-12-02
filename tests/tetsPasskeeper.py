88afb7751cb00ee6d9465204b5217636e4d869ad
Tatiana <75193605@continental.edu.pe>
2024-12-01 21:18:47 -0500

    Agregando pruebas unitarias de Inicio sesion

--- tests/tetsPasskeeper.py
+++ tests/tetsPasskeeper.py
@@ -1,8 +1,31 @@
 import unittest
 from src.database.inicio import verificar_credenciales
 
-
 class PruebasPassKeeper(unittest.TestCase):
+
+    def test_usuario_incorrecto(self):
+        usuario = "invalido"
+        contrasena = "1234"
+        resultado = verificar_credenciales(usuario, contrasena)
+        self.assertFalse(resultado, "Usuario incorrecto debería retornar False.")
+
+    def test_contrasena_incorrecta(self):
+        usuario = "admin"
+        contrasena = "wrongpassword"
+        resultado = verificar_credenciales(usuario, contrasena)
+        self.assertFalse(resultado, "Contraseña incorrecta debería retornar False.")
+
+    def test_usuario_y_contrasena_incorrectos(self):
+        usuario = "wronguser"
+        contrasena = "wrongpassword"
+        resultado = verificar_credenciales(usuario, contrasena)
+        self.assertFalse(resultado, "Usuario y contraseña incorrectos deberían retornar False.")
+
+    def test_caracteres_especiales(self):
+        usuario = "admin"
+        contrasena = "1234' OR '1'='1"
+        resultado = verificar_credenciales(usuario, contrasena)
+        self.assertFalse(resultado, "Inyección de SQL debería retornar False.")
 
     def test_credenciales_incorrectas(self):
         usuario = "123"
@@ -15,9 +38,6 @@ class PruebasPassKeeper(unittest.TestCase):
         contrasena = ""
         resultado = verificar_credenciales(usuario, contrasena)
         self.assertFalse(resultado, "Campos vacíos deberían retornar False.")
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
